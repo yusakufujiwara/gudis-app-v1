9cf94ffaf17c17c2565205c20f9cf3a3de95a530
yusaku@v02.itscom.net <yusaku@example.com>
2025-04-22 14:08:43 +0900

    Add Google site verification meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="ja">
       <Head>
         {/* Google Search Console 用の所有権確認タグ */}
-        <meta name="google-site-verification" content="l4m03Ly8DZd" />
+        <meta name="google-site-verification" content="I7zYDjgWp0RbJG7i0KnXDWzW-fQa1RP6loZ1WAz2eJI" />
       </Head>
       <body className="antialiased">
         <Main />
